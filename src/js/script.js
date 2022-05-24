7bfb97a1d82a5577df5de875f074b99dcfb0832f
Gustavo Monteiro <gu_cm@hotmail.com>
2022-05-24 15:53:20 -0300

    adicionado feedbacks no lugar dos alerts e responsivo

--- src/js/script.js
+++ src/js/script.js
@@ -29,9 +29,6 @@ const wordsBase = [
   { word: "armario", tip: "Objeto" },
   { word: "geladeira", tip: "Objeto" },
   { word: "sofa", tip: "Objeto" },
-  { word: "challenge", tip: "Desafio Alura" },
-  { word: "alura", tip: "Desafio Alura" },
-  { word: "oracle", tip: "Desafio Alura" },
 ];
 
 // variaveis do HTML
@@ -43,6 +40,7 @@ const buttonAddWordPage = document.getElementById("button-add-word-page");
 const buttonAddWord = document.getElementById("button-add-word");
 const buttonReplay = document.getElementById("button-replay");
 const buttonGiveUp = document.getElementById("button-giveup");
+const buttonTip = document.getElementById("button-tip");
 
 // funçionalidades do HTML
 // botão começar jogo
@@ -68,9 +66,21 @@ buttonAddWord.addEventListener("click", () => {
   createSlots(randomWord);
 });
 
+// botão mostrar dica
+buttonTip.addEventListener("click", () => {
+  const tipWord = document.querySelector(".forca-tip");
+  tipWord.classList.toggle("show");
+});
+
 // botão recomeçar jogo
 buttonReplay.addEventListener("click", () => {
   const partsBoneco = document.querySelectorAll(".boneco");
+  const tipWord = document.querySelector(".forca-tip");
+  const loser = document.querySelector(".loser");
+  const winner = document.querySelector(".winner");
+  winner.classList.remove("show-result");
+  loser.classList.remove("show-result");
+  tipWord.classList.remove("show");
   removeSlots();
   resetKeyboard();
   const randomWord = createRandomWord();
@@ -79,6 +89,7 @@ buttonReplay.addEventListener("click", () => {
   wordObject = [];
   erros = 0;
   acertos = 0;
+  venceu = false;
   let i;
   for (i = 0; i < 6; i++) {
     partsBoneco[i].classList.add("hide-boneco");
@@ -86,6 +97,7 @@ buttonReplay.addEventListener("click", () => {
 });
 
 // botão desistir do jogo
+let venceu = false;
 buttonGiveUp.addEventListener("click", () => {
   const spanWord = document.querySelectorAll(".word-placeholder");
   const word = hiddenWord.textContent;
@@ -93,6 +105,10 @@ buttonGiveUp.addEventListener("click", () => {
   let i;
   for (i = 0; i < word.length; i++) {
     spanWord[i].textContent = word[i].toUpperCase();
+  }
+  if (!venceu) {
+    const loser = document.querySelector(".loser");
+    loser.classList.add("show-result");
   }
 });
 
@@ -109,7 +125,7 @@ const createRandomWord = () => {
   const randomWord =
     wordsBase[Math.floor(Math.random() * (wordsBase.length - 1) + 1)];
   const tipWord = document.querySelector(".forca-tip");
-  tipWord.textContent = "Dica: " + randomWord.tip;
+  tipWord.textContent = randomWord.tip;
   hiddenWord.textContent = randomWord.word;
   return randomWord.word;
 };
@@ -172,7 +188,15 @@ function verifyLetter() {
     }
     usedKeys.push(keyPress);
   } else {
-    alert("voce ja usou essa letra");
+    const feedbackKey = document.querySelector(".feedback-key");
+    feedbackKey.classList.add("show-result");
+    setTimeout(() => {
+      feedbackKey.classList.add("hide-result");
+    }, 1500);
+    setTimeout(() => {
+      feedbackKey.classList.remove("show-result");
+      feedbackKey.classList.remove("hide-result");
+    }, 2000);
   }
 }
 
@@ -198,7 +222,9 @@ const correctKey = (key, word, keyPress) => {
   }
   acertos++;
   if (acertos == wordObject.length) {
-    alert("voce ganhou");
+    const winner = document.querySelector(".winner");
+    winner.classList.add("show-result");
+    venceu = true;
     endGame();
   }
 };
@@ -211,7 +237,8 @@ const wrongKey = (key) => {
   partsBoneco[erros].classList.remove("hide-boneco");
   erros++;
   if (erros == 6) {
-    alert("voce perdeu");
+    const loser = document.querySelector(".loser");
+    loser.classList.add("show-result");
     endGame();
   }
 };
